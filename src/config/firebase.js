f9fcc03408bea564886e2431bc9355dbe9de627c
Israel Lugo <hostelixisrael@gmail.com>
2018-03-05 10:08:08 -0400

    [ADD] add file config firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,6 @@
+export default {
+    "apiKey": "AIzaSyD44pLvfIx5i6S_h6LI9dMrxgTUZzE5UjQ",
+    "authDomain": "the-baltazartv-dota-1.firebaseapp.com",
+    "databaseURL": "https://the-baltazartv-dota-1.firebaseio.com",
+    "storageBucket": "gs://the-baltazartv-dota-1.appspot.com",
+};

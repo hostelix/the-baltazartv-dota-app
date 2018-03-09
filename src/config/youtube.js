cc9ca5664c978516ae54764ab9766bb327e3aa7a
Israel Lugo <hostelixisrael@gmail.com>
2018-03-09 10:10:52 -0400

    [MOD] add url api youtube

--- src/config/youtube.js
+++ src/config/youtube.js
@@ -1,3 +1,9 @@
-export default {
-	API_KEY: "IzaSyD44pLvfIx5i6S_h6LI9dMrxgTUZzE5UjQ"
+const YOUTUBE_URL_API = "https://www.googleapis.com/youtube/v3";
+const API_KEY = "AIzaSyD44pLvfIx5i6S_h6LI9dMrxgTUZzE5UjQ";
+const api = {
+    playListItems: `${YOUTUBE_URL_API}/playlistItems`,
+    search: `${YOUTUBE_URL_API}/search`,
+    channels: `${YOUTUBE_URL_API}/channels`
 };
+
+export {YOUTUBE_URL_API, API_KEY, api};

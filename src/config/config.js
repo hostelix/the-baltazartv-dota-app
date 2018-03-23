77c3a4470fb7995f08f1ed5028156c5738f5cfef
Israel Lugo <hostelixisrael@gmail.com>
2018-03-23 17:07:06 -0400

    [MOD] add playlistid and username

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,9 @@
-const username_youtube = "TheBaltazarTV";
+const PLAYLIST_ID = "UUo8fElL5iJdyf8gBP1ntjKA";
+const CHANNEL_ID = "UCo8fElL5iJdyf8gBP1ntjKA";
+const USERNAME_YOUTUBE = "TheBaltazarTV";
 
-export {username_youtube};
+export {
+    PLAYLIST_ID,
+    CHANNEL_ID,
+    USERNAME_YOUTUBE
+};

c553d5978f236b5cfff77d5843a4390c4a947ada
Israel Lugo <hostelixisrael@gmail.com>
2018-03-26 12:18:42 -0400

    [MOD] add property URLS to object api

--- src/config/youtube.js
+++ src/config/youtube.js
@@ -1,9 +1,11 @@
 const YOUTUBE_URL_API = "https://www.googleapis.com/youtube/v3";
 const API_KEY = "AIzaSyD44pLvfIx5i6S_h6LI9dMrxgTUZzE5UjQ";
 const api = {
-    playListItems: `${YOUTUBE_URL_API}/playlistItems`,
-    search: `${YOUTUBE_URL_API}/search`,
-    channels: `${YOUTUBE_URL_API}/channels`
+    URLS: {
+        playListItems: `${YOUTUBE_URL_API}/playlistItems`,
+        search: `${YOUTUBE_URL_API}/search`,
+        channels: `${YOUTUBE_URL_API}/channels`
+    }
 };
 
 export {YOUTUBE_URL_API, API_KEY, api};

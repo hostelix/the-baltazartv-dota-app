0dc24f67b3369a605e41482deb68319388b42e66
Israel Lugo <hostelixisrael@gmail.com>
2018-03-26 16:58:20 -0400

    [ADD] add data to menu

---
+++ src/data/menu.js
@@ -0,0 +1,22 @@
+const streaks = [
+    {key: "001", name: "Killing Spree"},
+    {key: "002", name: "Dominating"},
+    {key: "003", name: "Mega Kill"},
+    {key: "004", name: "Monster Kill"},
+    {key: "005", name: "Unstoppable"},
+    {key: "006", name: "Wicked Sick"},
+    {key: "007", name: "Godlike"},
+    {key: "008", name: "Beyond Godlike"}
+];
+
+const killCombos = [
+    {key: "009", name: "Double kill"},
+    {key: "010", name: "Triple Kill"},
+    {key: "011", name: "Ultra Kill"},
+    {key: "012", name: "Rampage"}
+];
+
+export {
+    streaks,
+    killCombos
+}

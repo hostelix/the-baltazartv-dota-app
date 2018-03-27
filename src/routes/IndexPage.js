1808404c742ad1040a6a8f0ff1ec00c4fa3add4c
Israel Lugo <hostelixisrael@gmail.com>
2018-03-27 13:59:44 -0400

    [MOD] add process to load video when select menu

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {connect} from 'dva';
-import {
-    Layout,
-    Divider,
-    Spin
-} from 'antd';
+import {Divider, Layout, Spin} from 'antd';
 import VideoList from '../components/VideoList';
 import PageHeader from '../components/Layout/PageHeader';
+import PageFooter from '../components/Layout/PageFooter';
 import * as styles from './IndexPage.css'
+import {killCombos, streaks} from "../data/menu";
+import * as _ from 'lodash';
 
-const {Content, Footer} = Layout;
+const {Content} = Layout;
+const data_menu = [...streaks, ...killCombos];
 
 
 class IndexPage extends React.Component {
@@ -34,51 +34,67 @@ class IndexPage extends React.Component {
         });
     }
 
-    handleClickMenu = (e) => {
-        console.log('click ', e);
-        this.setState({
-            currentMenu: e.key,
-        });
-    };
-
     handleSearch = (event) => {
         if (event.key === "Enter") {
-            this.setState({
-                query: event.target.value,
-                currentMenu: null
+            const {dispatch} = this.props;
+
+            const querySearch = event.target.value;
+
+            return dispatch({
+                type: 'video/search',
+                payload: {query: querySearch}
             });
         }
     };
 
-    handleSpinning(status) {
+    handleClickMenu = (event, keyMenuActive) => {
+        const {dispatch} = this.props;
+
+        if (keyMenuActive !== event.key) {
+            if (event.key === "home") {
+
+                return dispatch({
+                    type: 'video/fetch'
+                });
+            }
+            else {
+                const index = _.findIndex(data_menu, function (o) {
+                    return o.key === event.key;
+                });
+
+                if (index !== -1) {
+                    return dispatch({
+                        type: 'video/search',
+                        payload: {query: data_menu[index].name}
+                    });
+                }
+            }
+        }
+
+    };
+
+    handleSpinning = (status) => {
         this.setState({
             spinningActive: status
         });
-    }
-
-    createFooter = () => {
-        const current_year = new Date().getFullYear();
-        return `The BaltazarTv App ©${ current_year } Created by `;
     };
 
     render() {
         return (
             <Layout className="layout" style={{width: '100%', height: '100%'}}>
-                <PageHeader onSearch={this.handleSearch}/>
+                <PageHeader onSearch={this.handleSearch} onClickMenu={this.handleClickMenu}/>
+
                 <Divider/>
 
                 <Spin spinning={this.state.spinningActive} size={'large'}>
                     <Content className={styles.content}>
                         <div className={styles.contentDiv}>
-                            <VideoList spinning={this.handleSpinning} querySearch={this.state.query}/>
+                            <VideoList spinning={this.handleSpinning}/>
                         </div>
                     </Content>
                 </Spin>
 
-                <Footer style={{textAlign: 'center'}}>
-                    {this.createFooter()}
-                    <a href="http://github.com/hostelix">Hostelix ♥</a>
-                </Footer>
+                <PageFooter/>
             </Layout>
         );
     }
